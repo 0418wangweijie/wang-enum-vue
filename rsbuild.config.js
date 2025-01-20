5ec8b0165cfafb9dee868daa02e21ceb96d1db9e
王伟杰 <wangweijieyanfa@ctv.org.cn>
2025-01-20 20:45:57 +0800

    修改配置文件

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -6,10 +6,14 @@ import {pluginVue} from "@rsbuild/plugin-vue";
 export default defineConfig({
     plugins: [pluginVue()],
     output:{
+        target: 'node',
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
         library: 'wang-enum-vue',
         libraryTarget: 'umd',
+        // 明确只输出 js 文件
+        assetModuleFilename: 'js/[name].[hash].[ext]',
+        chunkFilename: 'js/[name].[chunkhash].js'
     },
     source: {
         entry: {
